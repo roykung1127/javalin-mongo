769653c551403488e48b3960ddd4a2fbfe1c9ca0
roykung1127 <roy18503@gmail.com>
2022-04-01 17:39:03 +0800

    add dependency javalin

---
+++ javalin-mongo/src/main/java/jar/HelloWorld.java
@@ -0,0 +1,10 @@
+package jar;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
